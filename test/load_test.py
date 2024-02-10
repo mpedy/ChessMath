662812c9a0a4108ea69c8cbd69a0e48c58860027
Matteo Provendola <82465468+mpedy@users.noreply.github.com>
2024-02-10 15:14:18 +0100

    Updated test

--- test/load_test.py
+++ test/load_test.py
@@ -1,14 +1,32 @@
 import time
 from locust import HttpUser, task
+import random
+from websockets.sync.client import connect
 
+letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 class Test(HttpUser):
     k = 0
-    def on_start(self):
-        self.client.get(f"/ws/853_ciao{self.k}")
-        self.k = self.k+1
+    def getRandomNames(self, k=3):
+        return ''.join(random.choices(letters, k=k))
+    #def on_start(self):
+    #    self.client.get(f"/ws/853_ciao{self.k}")
+    #    self.k = self.k+1
+
+    #@task
+    #def hello(self):
+    #    self.client.get(f"/ws/853_ehila_da_hello{self.k}")
+    #    self.k = self.k+1
     
     @task
-    def hello(self):
-        self.client.get(f"/ws/853_ehila{self.k}")
+    def login(self):
+        codice = 883
+        req = self.client.get(f"/verificacodice_{codice}", headers={"MyClientID" : str(self.k)})
+        if req.status_code == 200:
+            name = self.getRandomNames(k=10)
+            self.client.get(f"/inseriscinome_{name}", headers={"MyClientID" : str(self.k)})
+            with connect(f"ws://192.168.1.75:8000/ws/{codice}_{name}") as ws:
+                ws.send("ciao a tutti")
+                message = ws.recv()
+                print("Ricevuto: ",message)
         self.k = self.k+1
