de1c3f4575b35822b71b5f4eeab2e2b68d6fa47e
Matteo Provendola <82465468+mpedy@users.noreply.github.com>
2026-01-22 21:19:08 +0100

    Added backup dirs and fixed requirements

---
+++ static/js/med_a/img_spirit.js
@@ -0,0 +1,30 @@
+import { DrawChessboard as DrawChessboardClass } from "../drawchessboardnewnew.js";
+
+var drawChessboard = new DrawChessboardClass()
+
+drawChessboard.drawChessboard(document.getElementById("chessboard"))
+
+drawChessboard.piece_position = {
+    D5: "Knight.svg",
+}
+
+drawChessboard.drawPieces(document.getElementById("chessboard"), drawChessboard.piece_position)
+
+var cas = [B4, B6, C3, C7, E3, E7, F4, F6]
+
+for (var i in cas) {
+    var elem = $("#" + cas[i])[0]
+    var div = document.createElement("div");
+    div.style.background = "red"
+    div.style.borderRadius = "50%"
+    div.style.position = "absolute"
+    div.style.left = "10%"
+    div.style.top = "10%"
+    div.style.width = "80%"
+    div.style.height = "80%"
+    div.style.zIndex = "1";
+    elem.appendChild(div);
+}
+
+drawChessboard.handleMouseDown_casella = function () { }
+drawChessboard.handleMouseDown_image = function () { }
---
+++ static/js/med_a/quiz14.js
@@ -0,0 +1,27 @@
+import { DrawChessboard as DrawChessboardClass } from "../drawchessboardnewnew.js"
+import { MakeTimerClass } from "../maketimernew.js"
+import { getQuiz } from "../myui.js"
+
+var drawChessboard = new DrawChessboardClass()
+var maketimer = new MakeTimerClass()
+
+drawChessboard.drawChessboard($("#chessboard")[0])
+enlighted = "";
+enlight(C3, "orange", true);
+enlight(F5, "orange", true);
+can_answer = true
+maketimer.stopTimerFunction = function (pressed) {
+	try {
+		if (pressed == undefined) {
+			pressed = false;
+		}
+	} catch (errore) {
+		pressed = false;
+	}
+	if (!pressed) {
+		myalert("Tempo esaurito!", "E' scaduto il tempo!");
+	}
+	can_answer = false
+	$(".risposta").toggleClass("disabled")
+}
+getQuiz(maketimer);
