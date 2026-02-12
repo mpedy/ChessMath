c6c8159c97cfa661c79915f57ba724ca51120bfe
Matteo Provendola <82465468+mpedy@users.noreply.github.com>
2026-02-12 13:54:22 +0100

    Space

--- static/js/med/pages/gioco12b.js
+++ static/js/med/pages/gioco12b.js
@@ -4,7 +4,7 @@ var gioco12b = new MovingGame({
     piece_position: {
         "D5": "Knight.svg",
     },
-    moving_pieces: { "Knight": "D5"},
+    moving_pieces: { "Knight": "D5" },
     end_position: "E5",
     show_possible_moves: false,
     with_timer: true
